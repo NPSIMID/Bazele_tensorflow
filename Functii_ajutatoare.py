d91af87330751cd8e0d7cc09b5cedebe0bc978ab
NPSIMID <nicolaevpavel@gmail.com>
2021-11-03 17:54:54 +0200

    Actualiazarea functii ajutatoare

--- Functii_ajutatoare.py
+++ Functii_ajutatoare.py
@@ -106,11 +106,19 @@ def pred_and_plot(model, filename, class_names):
 import datetime
 import tensorflow as tf
 def create_tensorboard_callback(dir_name, experiment_name):
+  """
+  Creates a TensorBoard callback instand to store log files.
+  Stores log files with the filepath:
+    "dir_name/experiment_name/current_datetime/"
+  Args:
+    dir_name: target directory to store TensorBoard log files
+    experiment_name: name of experiment directory (e.g. efficientnet_model_1)
+  """
   log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-
-  tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir )
-
-  print(f"Fisierele de inregistrarea TensorBoard se salveaza in : {log_dir}")
+  tensorboard_callback = tf.keras.callbacks.TensorBoard(
+      log_dir=log_dir
+  )
+  print(f"Se salveaza datele inregistrate in directoriul: {log_dir}")
   return tensorboard_callback
 
 # Functia de crearea a modelelor cu extragerea caracteristicilor
